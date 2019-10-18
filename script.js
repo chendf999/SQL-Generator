b1d4e086aa3d4e101caff11615b8765afa36eb17
daphne <daphne.chen@autodesk.com>
2019-10-18 18:00:41 +0800

    wrapped into functions

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
-function getSQL(){
-  // default database
-  var database = 'desktop_product_intelligence_public.experiment_data_partitioned_weekly_v2';
+// default database
+var database = 'desktop_product_intelligence_public.experiment_data_partitioned_weekly_v2';
+var product;
+var component;
+var limit;
 
-  // get product name
-  var product =  `
+function getSQL(){
+  getComponent();
+  getProductAndDB();
+  getLimit();
+  output_code.innerHTML = product + component + limit;
+}
+
+function getProductAndDB(){
+  product =  `
   <li>SELECT *</li>
   <li>FROM ${database}</li>
   <li>AS dataset</li>
   <li>WHERE product_name = '${input_product.value}${input_release.value}'</li>
   `
+}
 
-  // get limit
-  var limit = input_limit.value;
+function getComponent(){
+  component = input_component.value;
+
+  if (component !== '') {
+    if (input_comp_waypoint.checked) {
+      database = 'desktop_product_usage_public.dap_experiment_details_daily';
+      component = `<li>AND component_name = '${input_component.value}'</li>`;
+    } else {
+      database = 'desktop_product_intelligence_public.experiment_data_partitioned_weekly_v2';
+      component = `<li>AND dataset.component_attributes['COMMON.COMMAND_NAME'] = '${input_component.value}'</li>`
+    }
+  }
+}
+
+function getLimit(){
+  limit = input_limit.value;
 
   if (limit > 0 && limit <= 5000) {
     limit = `<li>LIMIT ${limit}</li>`
@@ -20,19 +44,4 @@ function getSQL(){
     input_limit.value = '';
     return
   }
-
-  // render component
-  var component = input_component.value;
-
-  if (component !== '') {
-    if (input_comp_waypoint.checked) {
-      database = 'desktop_product_usage_public.dap_experiment_details_daily';
-      component = `<li>AND component_name = '${input_component.value}'</li>`
-    } else {
-      database = 'desktop_product_intelligence_public.experiment_data_partitioned_weekly_v2';
-      component = `<li>AND dataset.component_attributes['COMMON.COMMAND_NAME'] = '${input_component.value}'</li>`
-    }
-  }
-
-  output_code.innerHTML = product + component + '<li>ORDER BY dt DESC</li>' + limit;
 }
