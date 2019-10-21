4851cb60b3ab85b13896024308fb182a9ba105bf
daphne <daphne.chen@autodesk.com>
2019-10-21 15:05:23 +0800

    add date picker

--- script.js
+++ script.js
@@ -2,24 +2,26 @@
 var database = 'desktop_product_intelligence_public.experiment_data_partitioned_weekly_v2';
 var product;
 var component;
-var limit;
+var daterange;
+// var limit;
 
 function getSQL(){
   // check limit
-  limit = input_limit.value;
-
-  if (limit > 0 && limit <= 5000) {
-    limit = `<li>LIMIT ${limit}</li>`
-  } else {
-    alert('Please enter a number between 1-5000');
-    input_limit.value = '';
-    return
-  }
+  // limit = input_limit.value;
+  //
+  // if (limit > 0 && limit <= 5000) {
+  //   limit = `<li>LIMIT ${limit}</li>`
+  // } else {
+  //   alert('Please enter a number between 1-5000');
+  //   input_limit.value = '';
+  //   return
+  // }
 
   getComponent(); // command and waypoint use different db
   getProductAndDB();
-  
-  output_code.innerHTML = product + component + limit;
+  getDate();
+
+  output_code.innerHTML = product + component + daterange;
 }
 
 function getProductAndDB(){
@@ -43,4 +45,15 @@ function getComponent(){
       component = `<li>AND dataset.component_attributes['COMMON.COMMAND_NAME'] = '${input_component.value}'</li>`
     }
   }
+}
+
+function getDate(){
+  date = input_date.value.split(' ');
+  var start = date[0].replace(/\//g,'');
+  var end = date[2].replace(/\//g,'');
+
+  daterange = `
+  <li>AND dt BETWEEN ${start} AND ${end}</li>
+  `
+  console.log(start, end);
 }
